fb04f03668976d819a6bfaf48aeb859b040acf49
Jost1nVx7 <jostinjd2002@gmail.com>
2024-12-01 19:11:26 -0500

    subio

---
+++ src/main/java/espoch/edu/ec/automoviljtjc/AutomovilJTJC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.automoviljtjc;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class AutomovilJTJC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
